1b28686fff013460a94e7fdb9ef62dfd91a2ad91
Leo <102828023+Leo5561Dev@users.noreply.github.com>
2022-06-11 15:00:37 -0500

    Update SessionFactory.php

--- src/Leo/lobbycore/session/SessionFactory.php
+++ src/Leo/lobbycore/session/SessionFactory.php
@@ -1,1 +1,40 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Leo\lobbycore\session;
+
+use Leo\lobbycore\LobbyCore;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+
+class SessionFactory
+{
+    
+    /** @var Session[] */
+    private array $sessions = [];
+    
+    /**
+     * @return array
+     */
+    public function getSessions(): array
+    {
+        return $this->sessions;
+    }
+    
+    /**
+     * @param Player $player
+     */
+    public function createSession(Player $player): void
+    {
+        $this->sessions[$player->getName()] = new Session($player, SessionScoreboard::create($player, TextFormat::colorize(LobbyCore::getInstance()->getConfig()->get('scoreboard.title'))));
+    }
+    
+    /**
+     * @param Player $player
+     */
+    public function removeSession(Player $player): void
+    {
+        unset($this->sessions[$player->getName()]);
+    }
+}
