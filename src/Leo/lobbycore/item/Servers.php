94ae3e48978d066587b0a6fa06360029653cbb15
Leo <102828023+Leo5561Dev@users.noreply.github.com>
2022-06-11 14:57:53 -0500

    Update Servers.php

--- src/Leo/lobbycore/item/Servers.php
+++ src/Leo/lobbycore/item/Servers.php
@@ -1,1 +1,26 @@
+?php
 
+declare(strict_types=1);
+
+namespace Leo\lobbycore\item;
+
+use Leo\lobbycore\utils\Utils;
+use pocketmine\item\Compass;
+use pocketmine\item\ItemUseResult;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class Servers extends Compass
+{
+    
+    /**
+     * @param Player $player
+     * @param Vector3 $directionVector
+     * @return ItemUseResult
+     */
+    public function onClickAir(Player $player, Vector3 $directionVector): ItemUseResult
+    {
+        Utils::createMenuServers($player);
+		return ItemUseResult::SUCCESS();
+	}
+}
